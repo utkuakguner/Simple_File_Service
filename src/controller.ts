f9b68a0db8b5017dc4bcff5e66a19de515a24226
Utku Akgüner <utkuakguner2010@hotmail.com>
2024-12-09 11:43:00 +0300

    update

--- src/controller.ts
+++ src/controller.ts
@@ -4,8 +4,6 @@ import { Hono } from 'hono';
 import { PrismaClient } from '@prisma/client';
 import { cors } from 'hono/cors';
 import fileController from 'controller/file';
-import genericMiddleware from 'utils/middlewares/genericMiddleware';
-import localizationMiddleware from 'utils/middlewares/localizationMiddleware';
 import { serveStatic } from 'hono/bun';
 
 const controller = (app: Router, prisma: PrismaClient) => {
@@ -13,15 +11,11 @@ const controller = (app: Router, prisma: PrismaClient) => {
 
     mainRouter.use('*', cors());
 
-    mainRouter.use('*', genericMiddleware());
-
     mainRouter.get('/ping', async (c: Context) => c.text('Service online'));
 
     fileController(mainRouter, prisma);
 
     app.use('/uploads/*', serveStatic({ root: `./src` }));
-
-    app.use('*', localizationMiddleware);
 
     app.route('/api', mainRouter);
 };
