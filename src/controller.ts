b3269e32aef21641f0bbfd0e3a3923988aa50024
Utku Akgüner <utkuakguner2010@hotmail.com>
2025-01-15 11:10:13 +0300

    Update controller.ts

--- src/controller.ts
+++ src/controller.ts
@@ -15,6 +15,13 @@ const controller = (app: Router, prisma: PrismaClient) => {
 
     fileController(mainRouter, prisma);
 
+    app.use('/uploads/*', async (c: Context, next: Function) => {
+        const filePath = c.req.param('*');
+        const fileName = filePath.split('/').pop();
+        c.header('Content-Disposition', `attachment; filename="${fileName}"`);
+        await next();
+    });
+
     app.use('/uploads/*', serveStatic({ root: `./src` }));
 
     app.route('/api', mainRouter);
